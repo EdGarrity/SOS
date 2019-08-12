e010b8bbdc1b28b4738f902af9896417c4e04a0c
EdGarrity <30885192+EdGarrity@users.noreply.github.com>
2019-08-11 23:29:29 -0400

    Revert "Added Support for Additional Types of Error Functions"

--- SOS/Domain/ErrorFunction.h
+++ SOS/Domain/ErrorFunction.h
@@ -1,22 +1,29 @@
 #pragma once
-
 #include "..\PushGP\Individual.h"
 
 using namespace pushGP;
 
 namespace domain
 {
-	class ErrorFunction
-	{
+	//enum order_types
+	//{
+	//	buy = 1,
+	//	hold = 0,
+	//	sell = -1
+	//};
 
-	public:
-		// Evaluates an individual using the provided range
-		static double evaluate_individuals(static std::vector<int> & individual_indexes, static unsigned long input_start, static unsigned long input_end, unsigned int _test_case, bool _record_transactions) {};
+	// Evaluate an individual's program for a given day
+	order_types run_individual_program(static unsigned int individual_index, static unsigned long input_row);
 
-		static double lexicase_reproduction_selection_error_function(static int individual_index, static unsigned long input_start, static unsigned long input_end) {};
+	// Evaluates one day in a test case
+	void eval_one_day_of_test_case(static std::vector<int> & individual_indexes, static unsigned long input_row, unsigned int _test_case, bool _record_transactions);
 
-		// Evaluate an individual's program for all days from input_start to input_end
-		static double run_individuals_program(static int individual_index, static unsigned long input_start, static unsigned long input_end) {};
+	// Evaluates an individual using the provided range
+	double evaluate_individuals(static std::vector<int> & individual_indexes, static unsigned long input_start, static unsigned long input_end, unsigned int _test_case, bool _record_transactions);
 
-	};
+	double lexicase_reproduction_selection_error_function(static int individual_index, static unsigned long input_start, static unsigned long input_end);
+
+	// Evaluate an individual's program for all days from input_start to input_end
+	double run_individuals_program(static int individual_index, static unsigned long input_start, static unsigned long input_end);
+
 }
