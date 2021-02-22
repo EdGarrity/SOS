a643db038b5ed474b6923d1c8f9e9e3600525dab
EdGarrity <EdGarrity11375@gmail.com>
2021-02-22 14:22:06 -0500

    Ran release version for almost 29 generations.  Some memory leakage observed.  Will next run for 20,000.

--- SOS/Domain/Learn From Examples/Arguments.h
+++ SOS/Domain/Learn From Examples/Arguments.h
@@ -57,7 +57,7 @@ namespace domain
 		//		;; The instructions that pushgp will use in random code.
 
 		// Number of individuals in the population.
-		const unsigned int population_size = 2000; // 200'000;
+		const unsigned int population_size = 20000; // 200'000;
 
 		// The maximum number of generations to run GP.
 		const unsigned int max_generations_in_one_session = 10'000;
