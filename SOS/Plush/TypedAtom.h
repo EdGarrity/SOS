32fd4f36656a1310db66bbae38032595df523b7f
Edward Garrity <EGarrity@cleverdevices.com>
2022-04-23 12:24:26 -0400

    Removed TypedAtom class.

--- SOS/Plush/TypedAtom.h
+++ SOS/Plush/TypedAtom.h
@@ -1,16 +1,16 @@
-#pragma once
-#include "Plush.Instruction.h"
-#include "Plush.StaticInit.h"
-#include "Type.h"
-
-namespace Plush {
-
-	class TypedAtom : public Atom {
-
-	public:
-
-		virtual const Type& get_precondition() const = 0;
-		virtual const Type& get_postcondition() const = 0;
-
-	};
-}
+//#pragma once
+//#include "Plush.Instruction.h"
+//#include "Plush.StaticInit.h"
+//#include "Type.h"
+//
+//namespace Plush {
+//
+//	class TypedAtom : public Atom {
+//
+//	public:
+//
+//		virtual const Type& get_precondition() const = 0;
+//		virtual const Type& get_postcondition() const = 0;
+//
+//	};
+//}
