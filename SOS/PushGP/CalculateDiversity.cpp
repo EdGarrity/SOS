188431be330d01e51067c28ad5188b631ccbbda3
Edward Garrity <EdGarrity11375@gmail.com>
2022-08-06 17:34:38 -0400

    Added debugging statements in calculate_diversity().

--- SOS/PushGP/CalculateDiversity.cpp
+++ SOS/PushGP/CalculateDiversity.cpp
@@ -222,100 +222,160 @@ namespace pushGP
 	//
 	std::tuple<double, unsigned long> calculate_diversity()
 	{
-		// Used to calculate epsilon for all individuals
-		std::vector<double> test_case_errors;
-
-		// Calculate dynamic epsilon
-		double training_case_threashold = 0.0;
-		unsigned int non_zero_count = 0;
-
-		// Count of clusters that differed on at least 10 % of the training cases
-		unsigned int count_of_diverse_clusters = 0;
-
-		for (int case_index = 0; case_index < domain::argmap::number_of_training_cases; case_index++)
+		try
 		{
-			// Determine the threshold delta for this training case
-			test_case_errors.clear();
+			// Used to calculate epsilon for all individuals
+			std::vector<double> test_case_errors;
 
-			for (int individual_index = 0; individual_index < domain::argmap::population_size; individual_index++)
-				test_case_errors.push_back(pushGP::globals::error_matrix.load(case_index, individual_index));
+			// Calculate dynamic epsilon
+			double training_case_threashold = 0.0;
+			unsigned int non_zero_count = 0;
 
-			std::tie(training_case_threashold, non_zero_count) = mad_2(test_case_errors);
+			// Count of clusters that differed on at least 10 % of the training cases
+			unsigned int count_of_diverse_clusters = 0;
 
-			// Calculate the minimum error for this training case
-			double training_case_minimum_error = std::numeric_limits<double>::max();
 
-			for (int individual_index = 0; individual_index < domain::argmap::population_size; individual_index++)
+
+			std::cout << "Calculate Diversity() - Construct the elitized error vectors that indicate whether an individual achieved the best error on each training case" << std::endl;
+
+
+
+			for (int case_index = 0; case_index < domain::argmap::number_of_training_cases; case_index++)
 			{
-				double error = pushGP::globals::error_matrix.load(case_index, individual_index);
+				// Determine the threshold delta for this training case
+				test_case_errors.clear();
 
-				training_case_minimum_error = (error < training_case_minimum_error) ? error : training_case_minimum_error;
-			}
+				for (int individual_index = 0; individual_index < domain::argmap::population_size; individual_index++)
+					test_case_errors.push_back(pushGP::globals::error_matrix.load(case_index, individual_index));
 
-			// Set error threashold value
-			double error_threshold = training_case_minimum_error + training_case_threashold;
+				std::tie(training_case_threashold, non_zero_count) = mad_2(test_case_errors);
 
-			// Construct the “elitized” error vectors that indicate whether an individual achieved the best error on each training case
-			for (int individual_index = 0; individual_index < domain::argmap::population_size; individual_index++)
-			{
-				double error = pushGP::globals::error_matrix.load(case_index, individual_index);
+				// Calculate the minimum error for this training case
+				double training_case_minimum_error = std::numeric_limits<double>::max();
 
-				elitized[individual_index][case_index] = (error < error_threshold) ? 0 : 1;
-			}
-
-		}
-
-		// Initialize tree
-		for (int n = 0; n < domain::argmap::population_size; n++)
-		{
-			Cluster* cluster = new Cluster(elitized[n]);
-			tree[cluster->get_id()] = cluster;
-		}
-
-		// Grow tree
-		while (tree.size() > 1)
-		{
-			unsigned int cluster_1_index = Utilities::random_integer(tree.size());
-			auto it = tree.begin();
-			std::advance(it, cluster_1_index);
-			unsigned long cluster_1_key = it->first;
-
-			double min_dist = std::numeric_limits<double>::max();
-			unsigned int closest_cluster_key = 0;
-
-			for (auto it = tree.begin(); it != tree.end(); ++it)
-			{
-				unsigned int cluster_2_key = it->first;
-				if (cluster_1_key != cluster_2_key)
+				for (int individual_index = 0; individual_index < domain::argmap::population_size; individual_index++)
 				{
-					double dist = Cluster::get_distance(tree[cluster_1_key], tree[cluster_2_key]);
+					double error = pushGP::globals::error_matrix.load(case_index, individual_index);
 
-					if (dist < min_dist)
+					training_case_minimum_error = (error < training_case_minimum_error) ? error : training_case_minimum_error;
+				}
+
+				// Set error threashold value
+				double error_threshold = training_case_minimum_error + training_case_threashold;
+
+				// Construct the “elitized” error vectors that indicate whether an individual achieved the best error on each training case
+				for (int individual_index = 0; individual_index < domain::argmap::population_size; individual_index++)
+				{
+					double error = pushGP::globals::error_matrix.load(case_index, individual_index);
+
+					elitized[individual_index][case_index] = (error < error_threshold) ? 0 : 1;
+				}
+
+			}
+
+
+
+
+			std::cout << "Calculate Diversity() - Initialize tree" << std::endl;
+
+
+
+			// Initialize tree
+			for (int n = 0; n < domain::argmap::population_size; n++)
+			{
+				Cluster* cluster = new Cluster(elitized[n]);
+				tree[cluster->get_id()] = cluster;
+			}
+
+
+
+
+
+			std::cout << "Calculate Diversity() - Grow tree" << std::endl;
+
+
+
+			// Grow tree
+			while (tree.size() > 1)
+			{
+				unsigned int cluster_1_index = Utilities::random_integer(tree.size());
+				auto it = tree.begin();
+				std::advance(it, cluster_1_index);
+				unsigned long cluster_1_key = it->first;
+
+				double min_dist = std::numeric_limits<double>::max();
+				unsigned int closest_cluster_key = 0;
+
+				for (auto it = tree.begin(); it != tree.end(); ++it)
+				{
+					unsigned int cluster_2_key = it->first;
+					if (cluster_1_key != cluster_2_key)
 					{
-						min_dist = dist;
-						closest_cluster_key = cluster_2_key;
+						double dist = Cluster::get_distance(tree[cluster_1_key], tree[cluster_2_key]);
+
+						if (dist < min_dist)
+						{
+							min_dist = dist;
+							closest_cluster_key = cluster_2_key;
+						}
 					}
 				}
+
+				Cluster* cluster = new Cluster(tree[cluster_1_key], tree[closest_cluster_key], min_dist);
+				tree[cluster->get_id()] = cluster;
+
+				double cluster_diversity = (double)(domain::argmap::number_of_training_cases - cluster->get_failed_test_cases_count()) / (double)domain::argmap::number_of_training_cases;
+
+				if ((double)cluster->get_failed_test_cases_count() > ((double)domain::argmap::number_of_training_cases * domain::argmap::cluster_break_threshold))
+					count_of_diverse_clusters++;
+
+				delete tree[cluster_1_key];
+				delete tree[closest_cluster_key];
+
+				tree.erase(cluster_1_key);
+				tree.erase(closest_cluster_key);
 			}
 
-			Cluster* cluster = new Cluster(tree[cluster_1_key], tree[closest_cluster_key], min_dist);
-			tree[cluster->get_id()] = cluster;
 
-			double cluster_diversity = (double)(domain::argmap::number_of_training_cases - cluster->get_failed_test_cases_count()) / (double)domain::argmap::number_of_training_cases;
 
-			if ((double)cluster->get_failed_test_cases_count() > ((double)domain::argmap::number_of_training_cases * domain::argmap::cluster_break_threshold))
-				count_of_diverse_clusters++;
 
-			delete tree[cluster_1_key];
-			delete tree[closest_cluster_key];
 
-			tree.erase(cluster_1_key);
-			tree.erase(closest_cluster_key);
+			std::cout << "Calculate Diversity() - Clean up" << std::endl;
+
+
+
+			double dist = tree.begin()->second->get_distance() / (double)domain::argmap::number_of_training_cases;
+			delete tree.begin()->second;
+
+
+
+
+
+			std::cout << "Calculate Diversity() - Done" << std::endl;
+
+
+
+			return std::make_tuple(dist, count_of_diverse_clusters);
 		}
+		catch (const std::exception& e)
+		{
+			std::stringstream error;
 
-		double dist = tree.begin()->second->get_distance() / (double)domain::argmap::number_of_training_cases;
-		delete tree.begin()->second;
+			error << "Standard exception: " << e.what();
 
-		return std::make_tuple(dist, count_of_diverse_clusters);
+			std::cerr << error.str() << std::endl;
+
+			throw;
+		}
+		catch (...)
+		{
+			std::stringstream error;
+
+			error << "Exception occurred";
+
+			std::cerr << error.str() << std::endl;
+
+			throw;
+		}
 	}
 }
