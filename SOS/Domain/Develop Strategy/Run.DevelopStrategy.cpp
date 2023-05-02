7997ab6057831e9538cacb4810e3289dc841892a
Ed Garrity <edgarrity11375@gmail.com>
2023-05-01 22:35:39 -0400

    WIP

---
+++ SOS/Domain/Develop Strategy/Run.DevelopStrategy.cpp
@@ -0,0 +1,144 @@
+#define NOMINMAX
+
+#include <functional>
+#include <concurrent_unordered_set.h>
+#include <concurrent_vector.h>
+#include <deque>
+#include <ppl.h>
+#include <iostream>
+#include <limits>
+#include <set>
+#include <sstream>
+#include <string>
+#include <thread>
+#include <unordered_set>
+#include "Run.DevelopStrategy.h"
+#include "..\..\Database\SQLCommand.h"
+#include "..\..\Plush\Environment.h"
+#include "../../PushGP/Globals.h"
+
+namespace domain
+{
+	namespace develop_strategy
+	{
+		// Purpose: 
+		//   Load the financial data which the agnets will use to make trading decisions.
+		//
+		// Parameters:
+		//   None
+		// 
+		// Return value:
+		//   
+		//
+		// Side Effects:
+		//   None
+		//
+		// Thread Safe:
+		//   No
+		//
+		// Remarks:
+		//
+		void load_financial_data()
+		{
+		}
+
+		// Purpose: 
+		//   Load the financial data which the agnets will use to make trading decisions.
+		//
+		// Parameters:
+		//   None
+		// 
+		// Return value:
+		//   
+		//
+		// Side Effects:
+		//   None
+		//
+		// Thread Safe:
+		//   No
+		//
+		// Remarks:
+		//
+		void load_daily_close_data()
+		{
+		}
+
+		// Purpose: 
+		//   
+		//
+		// Parameters:
+		//   None
+		// 
+		// Return value:
+		//   
+		//
+		// Side Effects:
+		//   None
+		//
+		// Thread Safe:
+		//   No
+		//
+		// Remarks:
+		//
+		int run()
+		{
+			std::string debug_message;
+
+			database::SQLConnection con;
+
+			try
+			{
+				// ******************
+				// *** Initialize ***
+				// ******************
+
+				//Plush::Environment env;	// Commented out because it consumes a lot of memory on the stack.
+
+				//unsigned int generation_number = 1;
+				unsigned int generations_completed_this_session = 0;
+				bool done = false;
+
+				// Allocate meneory for Individuals (See https://stackoverflow.com/questions/19803162/array-size-error-x64-process)
+				size_t sz = domain::argmap::population_size;
+				pushGP::globals::population_agents = new pushGP::Individual[sz];
+				pushGP::globals::child_agents = new pushGP::Individual[sz];
+
+				// Initialize database connection
+				con.connect(argmap::db_init_datasource, argmap::db_init_catalog, argmap::db_user_id, argmap::db_user_password);
+
+				// Load data
+				load_financial_data();
+				load_daily_close_data();
+
+				while ((!done)
+					//&& (generation_number <= argmap::max_generations_in_one_session)
+					//&& (best_individual_score > 0.0)
+					)
+				{
+					// *****************************************************
+					// *** Calculate trading orders for each trading day ***
+					// *****************************************************
+
+					// ***********************
+					// *** Evaluate agents ***
+					// ***********************
+
+					// *********************
+					// *** Evolve agents ***
+					// *********************
+				}
+			}
+			catch (const std::exception& e)
+			{
+				std::cout << "Exception: " << e.what() << std::endl;
+				std::cout << "Debug message: " << debug_message << std::endl;
+			}
+			catch (...)
+			{
+				std::cout << "Unknown exception" << std::endl;
+				std::cout << "Debug message: " << debug_message << std::endl;
+			}
+			return 0;
+		}
+	}
+}
---
+++ SOS/Domain/Develop Strategy/Run.DevelopStrategy.h
@@ -0,0 +1,9 @@
+#pragma once
+
+namespace domain
+{
+	namespace develop_strategy
+	{
+		int run();
+	}
+}
