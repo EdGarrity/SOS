cc63cb651ab4c01c2187bed45e40291b4a4dd610
Ed Garrity <edgarrity11375@gmail.com>
2023-05-04 23:11:38 -0400

    WIP

--- SOS/Domain/Develop Strategy/Run.DevelopStrategy.cpp
+++ SOS/Domain/Develop Strategy/Run.DevelopStrategy.cpp
@@ -12,6 +12,8 @@
 #include <string>
 #include <thread>
 #include <unordered_set>
+#include <valarray>
+#include <map>
 #include "Run.DevelopStrategy.h"
 #include "..\..\Database\SQLCommand.h"
 #include "..\..\Plush\Environment.h"
@@ -24,6 +26,14 @@ namespace domain
 		database::SQLConnection con;
 
 		const std::string sqlstmt_sqlcmd_load_financial_data = "SELECT [Stock],CONVERT(varchar(25),[Date],120) AS [Date],[Key],[Value] FROM [SOS].[dbo].[TestData] ORDER BY [Stock],[Date],[Key];";
+
+		// define the dimensions of the array
+		const int STOCKS = 2;
+		const int DATES = 252;
+		const int ATTRIBUTES = 1765512;
+
+		// create the array and initialize all elements to zero
+		ThreeDimensionalArray testData(STOCKS, DATES, ATTRIBUTES);
 
 		// Purpose: 
 		//   Load the financial data which the agnets will use to make trading decisions.
@@ -43,7 +53,7 @@ namespace domain
 		//
 		// Remarks:
 		//
-		void load_training_financial_data()
+		void load_training_financial_data(ThreeDimensionalArray& testData)
 		{
 			std::cout << "Loading financial data..." << std::endl;
 
@@ -57,6 +67,11 @@ namespace domain
 				std::cout << "Stock Date Key Value" << std::endl;
 				std::cout << "--------------------" << std::endl;
 
+				// create a map to store the indexes of each stock, date, and attribute
+				std::map<std::string, size_t> stocks;
+				std::map<std::string, size_t> dates;
+				std::map<std::string, size_t> attributes;
+
 				sqlcmd_get_financial_data->execute();
 				while (sqlcmd_get_financial_data->fetch_next())
 				{
@@ -65,13 +80,45 @@ namespace domain
 					std::string key = sqlcmd_get_financial_data->get_field_as_string(3);
 					double value = sqlcmd_get_financial_data->get_field_as_double(4);
 
-					std::cout << stock << " " << date << " " << key << " " << value << std::endl;
+					stocks[stock] = 0;
+					dates[date] = 0;
+					attributes[key] = 0;
+
+					testData(stock, date, key) = value;
+				}
+
+				// set the indexes of each stock, date, and attribute
+				int i = 0;
+				for (auto& [key, value] : stocks) {
+					value = i++;
+				}
+				i = 0;
+				for (auto& [key, value] : dates) {
+					value = i++;
+				}
+				i = 0;
+				for (auto& [key, value] : attributes) {
+					value = i++;
+				}
+
+
+				// Print the array
+				for (int i = 0; i < STOCKS; i++)
+				{
+					for (int j = 0; j < DATES; j++)
+					{
+						for (int k = 0; k < ATTRIBUTES; k++)
+						{
+							std::cout << testData(i, j, k) << " ";
+						}
+						std::cout << std::endl;
+					}
+					std::cout << std::endl;
 				}
 			}
 			catch (const std::exception& e)
 			{
 				std::cout << "Exception: " << e.what() << std::endl;
-				//std::cout << "Debug message: " << sqlcmd_get_financial_data->get_debug_message() << std::endl;
 
 				delete sqlcmd_get_financial_data;
 
@@ -82,7 +129,6 @@ namespace domain
 			catch (...)
 			{
 				std::cout << "Unknown exception" << std::endl;
-				//std::cout << "Debug message: " << sqlcmd_get_financial_data->get_debug_message() << std::endl;
 
 				delete sqlcmd_get_financial_data;
 
@@ -183,7 +229,7 @@ namespace domain
 				con.connect(argmap::db_init_datasource, argmap::db_init_catalog, argmap::db_user_id, argmap::db_user_password);
 
 				// Load data
-				load_training_financial_data();
+				load_training_financial_data(testData);
 				load_daily_close_data();
 
 				while ((!done)
--- SOS/Domain/Develop Strategy/Run.DevelopStrategy.h
+++ SOS/Domain/Develop Strategy/Run.DevelopStrategy.h
@@ -4,6 +4,18 @@ namespace domain
 {
 	namespace develop_strategy
 	{
+		class ThreeDimensionalArray
+		{
+			std::valarray<double> data;
+			size_t x, y, z;
+
+		public:
+			class ThreeDimensionalArray(size_t x, size_t y, size_t z) : data(x * y * z), x(x), y(y), z(z) {}
+			double& operator()(size_t i, size_t j, size_t k) { return data[i + x * (j + y * k)]; }
+			double operator()(size_t i, size_t j, size_t k) const { return data[i + x * (j + y * k)]; }
+		};
+
+
 		int run();
 	}
 }
