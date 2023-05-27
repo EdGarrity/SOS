854e1211afc7974fdc7708be086fbc78e025fcda
Ed Garrity <30885192+EdGarrity@users.noreply.github.com>
2023-05-27 15:17:15 -0400

    Began work on the Breading functions.

--- SOS/Domain/Develop Strategy/Run.DevelopStrategy.cpp
+++ SOS/Domain/Develop Strategy/Run.DevelopStrategy.cpp
@@ -29,6 +29,151 @@ namespace domain
 {
 	namespace develop_strategy
 	{
+		concurrent_unordered_set<size_t> downsampled_training_cases;
+
+		// Purpose: 
+		//   
+		//
+		// Parameters:
+		//   None
+		// 
+		// Return value:
+		//   
+		//
+		// Side Effects:
+		//   None
+		//
+		// Thread Safe:
+		//   No
+		//
+		// Remarks:
+		//
+
+		/**
+		 * @brief This function produces new offspring.
+		 *
+		 * This function produces new offspring by breeding the current population of agents. The best individual is kept and the rest are bred using simulated annealing. The children are then mutated and regenerated as necessary.
+		 *
+		 * @param _number_of_example_cases The number of example cases.
+		 * @param _downsampled_training_cases The downsampled training cases.
+		 * @param _best_individual The best individual.
+		 * @param sa The simulated annealing object.
+		 * @param _include_best_individual_in_breeding_pool Whether or not to include the best individual in the breeding pool.
+		 *
+		 * @return void
+		 *
+		 * @remarks This function is not thread safe.
+		 */
+
+		void produce_new_offspring(unsigned long _number_of_example_cases,
+			concurrent_unordered_set<size_t>& _downsampled_training_cases,
+			unsigned long _best_individual,
+			pushGP::SimulatedAnnealing& sa,
+			bool _include_best_individual_in_breeding_pool)
+		{
+			try
+			{
+				std::set<std::string> set_of_gnomes;
+				combinable<pushGP::globals::Training_case_min_error_type> training_case_min_error;
+
+				// Reset children.
+				std::cout << "  Reset children" << std::endl;
+				for (unsigned long n = 0; n < argmap::population_size; n++)
+					pushGP::globals::child_agents[n].clear_genome();
+
+				// Breed new generation
+				std::cout << "  Breed new generation" << std::endl;
+
+				std::map<pushGP::SimulatedAnnealing_States, int> state_count;
+
+				state_count[pushGP::SimulatedAnnealing_States::alternate] = 0;
+				state_count[pushGP::SimulatedAnnealing_States::cloan] = 0;
+				state_count[pushGP::SimulatedAnnealing_States::mutate] = 0;
+				state_count[pushGP::SimulatedAnnealing_States::regenerate] = 0;
+
+				for (unsigned long individual_index = 0; individual_index < argmap::population_size; individual_index++)
+				{
+					Plush::Genome<Plush::CodeAtom>& child_genome = pushGP::globals::child_agents[individual_index].get_genome();
+
+					// Keep the best individual
+					if ((!_include_best_individual_in_breeding_pool) && (individual_index == _best_individual))
+						pushGP::globals::child_agents[individual_index].copy(pushGP::globals::population_agents[individual_index]);
+
+					else
+					{
+						if (individual_index % 100 == 0)
+							std::cout << "B";
+
+						pushGP::SimulatedAnnealing_States state = pushGP::breed(individual_index,
+							_number_of_example_cases,
+							_downsampled_training_cases,
+							training_case_min_error,
+							sa,
+							_include_best_individual_in_breeding_pool,
+							_best_individual);
+
+						state_count[state]++;
+
+						// If a child with the same genome already exists, create a new random child.
+						if (set_of_gnomes.insert(pushGP::globals::child_agents[individual_index].get_genome_as_string()).second == false)
+						{
+							pushGP::make_random_plush_genome(child_genome);
+						}
+					}
+				}
+
+				std::cout << std::endl;
+				std::cout << std::endl;
+
+				std::cout << "Selection distribution" << std::endl;
+				std::cout << "  Alternate = " << (double)state_count[pushGP::SimulatedAnnealing_States::alternate] / (double)argmap::population_size * 100.0 << std::endl;
+				std::cout << "  Alternate_elite = " << (double)state_count[pushGP::SimulatedAnnealing_States::alternate_elite] / (double)argmap::population_size * 100.0 << std::endl;
+				std::cout << "  Cloan = " << (double)state_count[pushGP::SimulatedAnnealing_States::cloan] / (double)argmap::population_size * 100.0 << std::endl;
+				std::cout << "  Mutate = " << (double)state_count[pushGP::SimulatedAnnealing_States::mutate] / (double)argmap::population_size * 100.0 << std::endl;
+				std::cout << "  Regenerate = " << (double)state_count[pushGP::SimulatedAnnealing_States::regenerate] / (double)argmap::population_size * 100.0 << std::endl;
+
+				std::cout << std::endl;
+				std::cout << std::endl;
+
+				// Keep the best individuals for each test case
+				std::cout << ".";
+				if (!_include_best_individual_in_breeding_pool)
+				{
+					for (unsigned long training_case = 0; training_case < domain::argmap::number_of_training_cases; training_case++)
+					{
+						unsigned long best_individual_for_training_case = training_case_min_error.local().individual_with_minimum_error_for_training_case[training_case];
+
+						if (best_individual_for_training_case < (std::numeric_limits<unsigned int>::max)())
+						{
+							pushGP::globals::child_agents[best_individual_for_training_case].copy(pushGP::globals::population_agents[best_individual_for_training_case]);
+						}
+					}
+				}
+
+				std::cout << std::endl;
+			}
+			catch (const std::exception& e)
+			{
+				std::stringstream error;
+
+				error << "Standard exception: " << e.what();
+
+				std::cerr << error.str() << std::endl;
+
+				throw;
+			}
+			catch (...)
+			{
+				std::stringstream error;
+
+				error << "Exception occurred";
+
+				std::cerr << error.str() << std::endl;
+
+				throw;
+			}
+		}
+
 		// Purpose: 
 		//   Use the PushP agent to generate the trading order for each trading day.
 		//
