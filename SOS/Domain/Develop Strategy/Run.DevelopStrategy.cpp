7586d32496959f600be799df716f32a5090d9b87
Ed Garrity <30885192+EdGarrity@users.noreply.github.com>
2023-05-29 19:00:49 -0400

    WIP

--- SOS/Domain/Develop Strategy/Run.DevelopStrategy.cpp
+++ SOS/Domain/Develop Strategy/Run.DevelopStrategy.cpp
@@ -171,13 +171,13 @@ namespace domain
 		}
 
 		// Purpose: 
-		//   Use the PushP agent to generate the trading order for each trading day.
+		//   Returns the number of the last run saved to the database.
 		//
 		// Parameters:
 		//   None
 		// 
 		// Return value:
-		//   
+		//   The number of the last run saved to the database or zero if no runs exist in the database.
 		//
 		// Side Effects:
 		//   None
@@ -187,12 +187,333 @@ namespace domain
 		//
 		// Remarks:
 		//
-		void generate_trading_orders()
+		unsigned long get_last_saved_run_number()
 		{
-			for (unsigned long individual_index = 0; individual_index < domain::argmap::population_size; individual_index++)
-			{
+			unsigned long n = 0;
 
-			}
+			database::SQLCommand* sqlcmd_get_last_saved_run_number;
+
+			sqlcmd_get_last_saved_run_number = new database::SQLCommand(&con, sqlstmt_get_last_saved_run_number);
+
+#if DLEVEL > 0
+			Utilities::debug_log(-1, "get_last_saved_generation_number", "sqlcmd");
+#endif
+
+			sqlcmd_get_last_saved_run_number->execute();
+
+			if (sqlcmd_get_last_saved_run_number->fetch_next())
+				n = sqlcmd_get_last_saved_run_number->get_field_as_long(1);
+
+			delete sqlcmd_get_last_saved_run_number;
+
+			return ((n >= 0) || (n < 1000000)) ? n : 0;
+		}
+
+		// Purpose: 
+		//   Returns the number of the last generation saved to the database.
+		//
+		// Parameters:
+		//   None
+		// 
+		// Return value:
+		//   The number of the last generation saved to the database or zero if no generations exist in the database.
+		//
+		// Side Effects:
+		//   None
+		//
+		// Thread Safe:
+		//   No
+		//
+		// Remarks:
+		//
+		unsigned long get_last_saved_generation_number()
+		{
+			unsigned long n = 0;
+
+			database::SQLCommand* sqlcmd_get_last_saved_generation_number;
+
+			sqlcmd_get_last_saved_generation_number = new database::SQLCommand(&con, sqlstmt_get_last_saved_generation_number);
+
+#if DLEVEL > 0
+			Utilities::debug_log(-1, "get_last_saved_generation_number", "sqlcmd");
+#endif
+
+			sqlcmd_get_last_saved_generation_number->execute();
+
+			if (sqlcmd_get_last_saved_generation_number->fetch_next())
+				n = sqlcmd_get_last_saved_generation_number->get_field_as_long(1);
+
+			delete sqlcmd_get_last_saved_generation_number;
+
+			return ((n >= 0) || (n < 1000000)) ? n : 0;
+		}
+
+		// Purpose: 
+		//   Returns the SA tempreture of the last generation saved to the database.
+		//
+		// Parameters:
+		//   _default_temperature	-	The default to return if database table is empty
+		// 
+		// Return value:
+		//   The SA tempreture or the default tempreture if table in database is empty
+		//
+		// Side Effects:
+		//   None
+		//
+		// Thread Safe:
+		//   No
+		//
+		// Remarks:
+		//
+		double get_last_saved_temperature(double _default_temperature)
+		{
+			double n = _default_temperature;
+
+			database::SQLCommand* sqlcmd_get_last_saved_temperature;
+
+			sqlcmd_get_last_saved_temperature = new database::SQLCommand(&con, sqlstmt_get_last_saved_temperature);
+
+#if DLEVEL > 0
+			Utilities::debug_log(-1, "get_last_saved_temperature", "sqlcmd");
+#endif
+
+			sqlcmd_get_last_saved_temperature->execute();
+
+			if (sqlcmd_get_last_saved_temperature->fetch_next())
+				n = sqlcmd_get_last_saved_temperature->get_field_as_double(1);
+
+			delete sqlcmd_get_last_saved_temperature;
+
+			return n;
+		}
+
+		// Purpose: 
+		//   Returns the score of the best individual from the database, where:
+		//		score := ratio of test case errors / total test cases.
+		//
+		// Parameters:
+		//   _default_score	-	The default to return if database table is empty
+		// 
+		// Return value:
+		//   The error of the best individual from the database
+		//
+		// Side Effects:
+		//   None
+		//
+		// Thread Safe:
+		//   No
+		//
+		// Remarks:
+		//
+		double get_last_best_individual_score(double _default_score)
+		{
+			double n = _default_score;
+
+			database::SQLCommand* sqlcmd_get_last_best_individual_score;
+
+			sqlcmd_get_last_best_individual_score = new database::SQLCommand(&con, sqlstmt_get_last_best_individual_score);
+
+			sqlcmd_get_last_best_individual_score->execute();
+
+			if (sqlcmd_get_last_best_individual_score->fetch_next())
+				n = sqlcmd_get_last_best_individual_score->get_field_as_double(1);
+
+			delete sqlcmd_get_last_best_individual_score;
+
+			return n;
+		}
+
+		// Purpose: 
+		//   Returns the error of the best individual from the database, where:
+		//		error := average of the vector RMS difference between the example vector and the program's output vector for all examples in a training or test case
+		//
+		// Parameters:
+		//   _default_error	-	The default to return if database table is empty
+		// 
+		// Return value:
+		//   The error of the best individual from the database
+		//
+		// Side Effects:
+		//   None
+		//
+		// Thread Safe:
+		//   No
+		//
+		// Remarks:
+		//
+		double get_last_best_individual_error(double _default_error)
+		{
+			double n = _default_error;
+
+			database::SQLCommand* sqlcmd_get_last_best_individual_error;
+
+			sqlcmd_get_last_best_individual_error = new database::SQLCommand(&con, sqlstmt_get_last_best_individual_error);
+
+#if DLEVEL > 0
+			Utilities::debug_log(-1, "get_last_best_individual_error", "sqlcmd");
+#endif
+			sqlcmd_get_last_best_individual_error->execute();
+
+			if (sqlcmd_get_last_best_individual_error->fetch_next())
+				n = sqlcmd_get_last_best_individual_error->get_field_as_double(1);
+
+			delete sqlcmd_get_last_best_individual_error;
+
+			return n;
+		}
+
+		// Purpose: 
+		//   Returns the error of the 2nd best individual from the database, where:
+		//		error := average of the vector RMS difference between the example vector and the program's output vector for all examples in a training or test case
+		//
+		// Parameters:
+		//   _default_error	-	The default to return if database table is empty
+		// 
+		// Return value:
+		//   The error of the 2nd best individual from the database
+		//
+		// Side Effects:
+		//   None
+		//
+		// Thread Safe:
+		//   No
+		//
+		// Remarks:
+		//
+		double get_last_prev_best_individual_error(double _default_error)
+		{
+			double n = _default_error;
+
+			database::SQLCommand* sqlcmd_get_last_prev_best_individual_error;
+
+			sqlcmd_get_last_prev_best_individual_error = new database::SQLCommand(&con, sqlstmt_get_last_prev_best_individual_error);
+
+#if DLEVEL > 0
+			Utilities::debug_log(-1, "get_last_prev_best_individual_error", "sqlcmd");
+#endif
+			sqlcmd_get_last_prev_best_individual_error->execute();
+
+			if (sqlcmd_get_last_prev_best_individual_error->fetch_next())
+				n = sqlcmd_get_last_prev_best_individual_error->get_field_as_double(1);
+
+			delete sqlcmd_get_last_prev_best_individual_error;
+
+			return n;
+		}
+
+		// Purpose: 
+		//   Returns the stalled count from the database
+		//
+		// Parameters:
+		//   _default_stalled_count	-	The default to return if database table is empty
+		// 
+		// Return value:
+		//   The stalled count
+		//
+		// Side Effects:
+		//   None
+		//
+		// Thread Safe:
+		//   No
+		//
+		// Remarks:
+		//
+		unsigned long get_last_stalled_count(unsigned long _default_stalled_count)
+		{
+			unsigned long n = _default_stalled_count;
+
+			database::SQLCommand* sqlcmd_get_last_stalled_count;
+
+			sqlcmd_get_last_stalled_count = new database::SQLCommand(&con, sqlstmt_get_last_stalled_count);
+
+#if DLEVEL > 0
+			Utilities::debug_log(-1, "get_last_stalled_count", "sqlcmd");
+#endif
+			sqlcmd_get_last_stalled_count->execute();
+
+			if (sqlcmd_get_last_stalled_count->fetch_next())
+				n = sqlcmd_get_last_stalled_count->get_field_as_long(1);
+
+			delete sqlcmd_get_last_stalled_count;
+
+			return n;
+		}
+
+		// Purpose: 
+		//   Returns the cool down count from the database
+		//
+		// Parameters:
+		//   _default_cool_down_count	-	The default to return if database table is empty
+		// 
+		// Return value:
+		//   The cool down count
+		//
+		// Side Effects:
+		//   None
+		//
+		// Thread Safe:
+		//   No
+		//
+		// Remarks:
+		//
+		unsigned long get_last_cool_down_count(unsigned long _default_cool_down_count)
+		{
+			unsigned long n = _default_cool_down_count;
+
+			database::SQLCommand* sqlcmd_get_last_cool_down_count;
+
+			sqlcmd_get_last_cool_down_count = new database::SQLCommand(&con, sqlstmt_get_last_cool_down_count);
+
+#if DLEVEL > 0
+			Utilities::debug_log(-1, "get_last_cool_down_count", "sqlcmd");
+#endif
+			sqlcmd_get_last_cool_down_count->execute();
+
+			if (sqlcmd_get_last_cool_down_count->fetch_next())
+				n = sqlcmd_get_last_cool_down_count->get_field_as_long(1);
+
+			delete sqlcmd_get_last_cool_down_count;
+
+			return n;
+		}
+
+
+		// Purpose: 
+		//   Returns the Include the best individual in breeding flag
+		//
+		// Parameters:
+		//   _default_cool_down_count	-	The default to return if database table is empty
+		// 
+		// Return value:
+		//   Include the best individual in breeding flag
+		//
+		// Side Effects:
+		//   None
+		//
+		// Thread Safe:
+		//   No
+		//
+		// Remarks:
+		//
+		bool get_include_best_individual_in_breeding_pool(unsigned long _default_include_best_individual_in_breeding_pool)
+		{
+			bool n = _default_include_best_individual_in_breeding_pool;
+
+			database::SQLCommand* sqlcmd_get_include_best_individual_in_breeding_pool;
+
+			sqlcmd_get_include_best_individual_in_breeding_pool = new database::SQLCommand(&con, sqlstmt_get_include_best_individual_in_breeding_pool);
+
+#if DLEVEL > 0
+			Utilities::debug_log(-1, "get_include_best_individual_in_breeding_pool", "sqlcmd");
+#endif
+			sqlcmd_get_include_best_individual_in_breeding_pool->execute();
+
+			if (sqlcmd_get_include_best_individual_in_breeding_pool->fetch_next())
+				n = sqlcmd_get_include_best_individual_in_breeding_pool->get_field_as_long(1);
+
+			delete sqlcmd_get_include_best_individual_in_breeding_pool;
+
+			return n;
 		}
 
 
@@ -234,6 +555,36 @@ namespace domain
 				pushGP::globals::population_agents = new pushGP::Individual[sz];
 				pushGP::globals::child_agents = new pushGP::Individual[sz];
 
+				// Load data from most recent database record
+				unsigned int run_number = get_last_saved_run_number();
+				//unsigned int generation_number = get_last_saved_generation_number() + 1;
+				double best_individual_score = get_last_best_individual_score(std::numeric_limits<double>::max());
+				//double best_individual_error = get_last_best_individual_error(std::numeric_limits<double>::max());
+				//double prev_best_individual_error = get_last_prev_best_individual_error(std::numeric_limits<double>::max());
+				int stalled_count = get_last_stalled_count(argmap::stalled_count_trigger);
+				int cool_down_count = get_last_cool_down_count(argmap::cool_down_period);
+				bool include_best_individual_in_breeding_pool = get_include_best_individual_in_breeding_pool(true);
+
+				sa.set_cold();
+				sa.set_temperature(get_last_saved_temperature(sa.get_temperature()));
+
+				// If last run found a solution or exhausted the number of generations, 
+				// then clear the individuals table to force the creation of new individuals
+				// and reset to start a new run
+				if ((best_individual_score <= 0.0) || (generation_number > argmap::max_generations_in_one_session))
+				{
+					run_number++;
+					generation_number = 1;
+					best_individual_score = std::numeric_limits<double>::max();
+					//best_individual_error = std::numeric_limits<double>::max();
+					//prev_best_individual_error = std::numeric_limits<double>::max();
+					sa.set_temperature(0);
+					cool_down_count = argmap::cool_down_period;
+					stalled_count = argmap::stalled_count_trigger;
+					include_best_individual_in_breeding_pool = true;
+					//clear_individuals_table();
+				}
+
 				// Load data
 				datastore::case_data.load();
 
@@ -242,6 +593,35 @@ namespace domain
 					//&& (best_individual_score > 0.0)
 					)
 				{
+					// *****************************************************
+					// *** Simulated annealing  ***
+					// *****************************************************
+					if (cool_down_count <= 0)
+					{
+						stalled_count = (stalled_count < 0) ? 0 : stalled_count - 1;
+
+						if (stalled_count <= 0)
+						{
+							sa.set_temperature(std::min(best_individual_score, 1.0));
+
+							cool_down_count = argmap::cool_down_period;
+							include_best_individual_in_breeding_pool = false;
+
+							std::cout << "Heat up " << sa.get_temperature() << std::endl;
+						}
+					}
+
+					else
+					{
+						sa.cool_down();
+						stalled_count = argmap::stalled_count_trigger;
+
+						cool_down_count = (cool_down_count < 0) ? 0 : cool_down_count - 1;
+						include_best_individual_in_breeding_pool = true;
+
+						std::cout << "Cool down " << sa.get_temperature() << std::endl;
+					}
+
 					// *****************************************************
 					// *** Calculate trading orders for each trading day ***
 					// *****************************************************
@@ -260,6 +640,8 @@ namespace domain
 					// *** Evaluate strategies ***
 					// ***************************
 					Utilities::ThreadSafeArray_2D_V2<Trader> trader(domain::argmap::population_size, datastore::case_data.get_number_of_cases());
+					int best_individual = -1;
+					//double best_individual_score = std::numeric_limits<double>::min();
 
 					for (size_t training_case_index = 0; 
 						training_case_index < (datastore::case_data.get_number_of_cases() - domain::argmap::training_case_length); 
@@ -277,6 +659,14 @@ namespace domain
 								size_t stock_price_index = training_case_index + trading_case;
 
 								(trader.load(strategy_index, training_case_index)).execute(stock_price_index, order);
+
+								double score = trader.load(strategy_index, training_case_index).unrealized_value();
+
+								if (best_individual_score < score)
+								{
+									best_individual_score = score;
+									best_individual = strategy_index;
+								}
 							}
 						}
 					}
